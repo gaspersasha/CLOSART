b4e9ae4953d7bae1ff4c2dd5d7e0489672633c71
gaspersasha <gaspersasha@gmail.com>
2019-06-30 14:42:11 +0300

    Express

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-const https = require('http');
-const routes = require('./routes');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = https.createServer(routes);
+const adminRouts = require('./routes/admin');
+const shopRouts = require('./routes/shop');
 
-server.listen(3030);
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRouts);
+app.use(shopRouts);
+
+app.use('/', (req, res, next) => {
+    res.status(400).send('<h1>not found</h1>');
+});
+
+app.listen(3030);
